351e509a17d6326648cd7cdbfc8cf9cb36bb366f
andrew <a@a.cp>
2017-08-10 12:40:53 -0700

    added public dir and test pages

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 const express = require('express');
 const fs = require('fs');
 
+const port = 3000;
 let app = express();
 
+app.use(express.static(__dirname + '/public'));
+
+app.get('/de.html', (req, res) => {
+    res.render('./public/de.html');
+});
+
+app.get('/help', (req, res) => {
+    res.send('<p>TESTING</p>');
+});
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
+});
