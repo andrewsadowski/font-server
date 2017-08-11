f075da2e852c932f91ef1ef820ebe69c070c9b55
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2017-08-10 18:39:13 -0700

    added locale support in server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/home', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/public/html/home.html'));
 });
 
@@ -16,6 +16,43 @@ app.get('/html/de.html', (req, res) => {
         pageTitle: 'DE'
     });
 });
+
+app.get('/html/fr.html', (req, res) => {
+    res.render('/public/html/fr.html', {
+        pageTitle: 'FR'
+    });
+});
+
+app.get('/html/es_la.html', (req, res) => {
+    res.render('/public/html/es_la.html', {
+        pageTitle: 'esLA'
+    });
+});
+
+app.get('/html/ko.html', (req, res) => {
+    res.render('/public/html/ko.html', {
+        pageTitle: 'KO'
+    });
+});
+
+app.get('/html/th.html', (req, res) => {
+    res.render('/public/html/th.html', {
+        pageTitle: 'TH'
+    });
+});
+
+app.get('/html/zh_cn.html', (req, res) => {
+    res.render('/public/html/zh_cn.html', {
+        pageTitle: 'zhCN'
+    });
+});
+
+app.get('/html/zh_tw.html', (req, res) => {
+    res.render('/public/html/zh_tw.html', {
+        pageTitle: 'zhTW'
+    });
+});
+
 
 app.get('/help', (req, res) => {
     res.send('<p>TESTING</p>');
