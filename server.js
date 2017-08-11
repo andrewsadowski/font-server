bc0510b6ed87cae8d4687c571ae1fad099b0d614
Admin <ethan.shields21@gmail.com>
2017-08-11 13:50:57 -0700

    Undid local link changes

--- server.js
+++ server.js
@@ -11,43 +11,43 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/public/html/home.html'));
 });
 
-app.get('/de.html', (req, res) => {
+app.get('/html/de.html', (req, res) => {
     res.render('/public/html/de.html', {
         pageTitle: 'DE'
     });
 });
 
-app.get('/fr.html', (req, res) => {
+app.get('/html/fr.html', (req, res) => {
     res.render('/public/html/fr.html', {
         pageTitle: 'FR'
     });
 });
 
-app.get('/es_la.html', (req, res) => {
+app.get('/html/es_la.html', (req, res) => {
     res.render('/public/html/es_la.html', {
         pageTitle: 'esLA'
     });
 });
 
-app.get('/ko.html', (req, res) => {
+app.get('/html/ko.html', (req, res) => {
     res.render('/public/html/ko.html', {
         pageTitle: 'KO'
     });
 });
 
-app.get('/th.html', (req, res) => {
+app.get('/html/th.html', (req, res) => {
     res.render('/public/html/th.html', {
         pageTitle: 'TH'
     });
 });
 
-app.get('/zh_cn.html', (req, res) => {
+app.get('/html/zh_cn.html', (req, res) => {
     res.render('/public/html/zh_cn.html', {
         pageTitle: 'zhCN'
     });
 });
 
-app.get('/zh_tw.html', (req, res) => {
+app.get('/html/zh_tw.html', (req, res) => {
     res.render('/public/html/zh_tw.html', {
         pageTitle: 'zhTW'
     });
